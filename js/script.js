378bbffeda066317cd00da35ce5a9ccebae155f0
teenage1ove <kirillstars7@gmail.com>
2024-03-07 13:16:41 +0300

    Add function fetchCodes

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ import state from "./state.js";
 const {selects, success} = variables
 
 
+
+const fetchCodes = async () => {
+    try {
+        const response = await fetch(`${state.url}/codes`)
+        const data = await response.json()
+        
+        if (data.result === success) {
+            state.codes = data.supported_codes
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+fetchCodes()
